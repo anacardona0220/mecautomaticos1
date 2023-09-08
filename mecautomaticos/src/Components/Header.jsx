d9d6088312e2be79f11b535d75544fb8aee3d2f8
anacardona0220 <anamariacb0220@gmail.com>
2023-09-08 16:09:49 -0500

    update

--- mecautomaticos/src/Components/Header.jsx
+++ mecautomaticos/src/Components/Header.jsx
@@ -1,8 +1,37 @@
 const Header = () => {
   return (
-    <div>
+    <div className="header">
+      <div className="container container-header">
+        <div className="barra" />
+          <div className="logo">
+            <h1 className="nombre-sitio">
+            Mecautomaticos <span>S.A.S</span>
+            </h1>
+          </div>
+          <div className="contacto">
+            <a href="tel:3136789101">3136789101</a>
+            <nav className="navegacion">
+            <ul>
+
+              <li className="list" href="#">Nosotros</li>
+              <li className="list" href="#">Servicios</li>
+              <li className="list" href="#">Proyectos</li>
+              <li className="list" href="#">Contactanos</li>
+            </ul>
+              
+            </nav>
+
+
+          </div>
+        <div />
+      </div>
+      <div className="slogan">
+      <h1>Automatizando el futuro</h1>
+      <p>Soluciones inteligentes para la industria</p>
+
+      </div>
       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShVd0xngEMMtA5DN6tRPdb0j98y-BZnZXzpQ&usqp=CAU" />
-      <h1>Mecautomaticos S.A.S</h1>
+      
     </div>
   );
 };
--- mecautomaticos/src/Components/Servicios.jsx
+++ mecautomaticos/src/Components/Servicios.jsx
@@ -23,82 +23,89 @@ const Servicios = () => {
               height="300"
             />
           </div>
-          
-            <div className="list-2-2">
-              <li>Electrónica Industrial</li>
-              <p>
-                Ofrecemos soluciones avanzadas en electrónica industrial,
-                incluyendo diseño de circuitos electrónicos, desarrollo de
-                sistemas de control y monitoreo, y reparación de equipos
-                electrónicos industriales. Nuestro enfoque en la electrónica
-                gara
-              </p>
-              <img
-                className="box"
-                src="https://img.freepik.com/free-photo/person-using-ai-tool-job_23-2150711477.jpg?w=900&t=st=1694124390~exp=1694124990~hmac=d6bef4e081f8f995977cdd9439720985f6afaa7a3638afdbc9db27e1a3a6e39e"
-                alt="electronica"
-                width="300"
-                height="300"
-              />
-            </div>
-            <div className="list-2-3">
-              <li>Programación de PLC (Controladores Lógicos Programables)</li>
-              <p>
-                Nuestros expertos en programación de PLC crean lógica de control
-                personalizada para máquinas y procesos industriales. Utilizamos
-                PLC de última generación para garantizar un control preciso y
-                una respuesta rápida en entornos industriales exigentes
-              </p>
-              <img
-                className="box"
-                src="https://img.freepik.com/free-photo/electronic-circuit-board_1398-1073.jpg?w=826&t=st=1694125121~exp=1694125721~hmac=f16e8905c2ec453149d0eb44399c4d1364beaca109d5a0ff07fc0e6a108a508b"
-                alt="plc"
-                width="300"
-                height="300"
-              />
-            </div>
 
-            <div className="list-2-4">
-              <li>Montajes Industriales</li>
-              <p>
-                Proporcionamos servicios completos de montaje industrial para la
-                instalación y configuración de maquinaria y sistemas en plantas
-                de producción. Nuestra experiencia garantiza una implementación
-                sin problemas y una puesta en marcha eficiente
-              </p>
-              <li>Montajes Metalmecánicos</li>
-              <p>
-                Realizamos montajes metalmecánicos de alta calidad para
-                estructuras industriales, equipos y sistemas. Nuestros servicios
-                incluyen diseño, fabricación y montaje de componentes metálicos,
-                asegurando la resistencia y durabilidad de las instalaciones.
-              </p>
-              <img
-                className="box"
-                src="https://img.freepik.com/free-photo/interior-view-steel-factory_1359-117.jpg?w=826&t=st=1694125028~exp=1694125628~hmac=d07535b0363906a945700a65ff690b444eec69da12085616eb231f5565b6b84a"
-                alt="montajes"
-                width="300"
-                height="300"
-              />
-            </div>
-            <div className="list-2-5">
-              <li>Puertas Eléctricas y Control de Acceso</li>
-              <p>
-                nstalamos sistemas de puertas eléctricas y control de acceso
-                para empresas e instalaciones industriales. Esto incluye puertas
-                automáticas, portones de seguridad y sistemas de acceso con
-                tarjeta o biométricos para garantizar la seguridad y la gestión
-                eficiente del flujo de personas.
-              </p>
-              <img
-                className="box"
-                src="https://img.freepik.com/free-photo/intercom-gate-front-residential-building_637285-1905.jpg?w=826&t=st=1694125294~exp=1694125894~hmac=fc7b2f24746c332c3f98a67d4a28b4407b53d09b9bb97d424ae9756fdeef0186"
-                alt="puerta"
-                width="300"
-                height="300"
-              />
-            </div>
+          <div className="list-2-2">
+            <li>Electrónica Industrial</li>
+            <p>
+              Ofrecemos soluciones avanzadas en electrónica industrial,
+              incluyendo diseño de circuitos electrónicos, desarrollo de
+              sistemas de control y monitoreo, y reparación de equipos
+              electrónicos industriales. Nuestro enfoque en la electrónica gara
+            </p>
+            <img
+              className="box"
+              src="https://img.freepik.com/free-photo/person-using-ai-tool-job_23-2150711477.jpg?w=900&t=st=1694124390~exp=1694124990~hmac=d6bef4e081f8f995977cdd9439720985f6afaa7a3638afdbc9db27e1a3a6e39e"
+              alt="electronica"
+              width="300"
+              height="300"
+            />
+          </div>
+          <div className="list-2-3">
+            <li>Programación de PLC (Controladores Lógicos Programables)</li>
+            <p>
+              Nuestros expertos en programación de PLC crean lógica de control
+              personalizada para máquinas y procesos industriales. Utilizamos
+              PLC de última generación para garantizar un control preciso y una
+              respuesta rápida en entornos industriales exigentes
+            </p>
+            <img
+              className="box"
+              src="https://img.freepik.com/free-photo/electronic-circuit-board_1398-1073.jpg?w=826&t=st=1694125121~exp=1694125721~hmac=f16e8905c2ec453149d0eb44399c4d1364beaca109d5a0ff07fc0e6a108a508b"
+              alt="plc"
+              width="300"
+              height="300"
+            />
+          </div>
 
+          <div className="list-2-4">
+            <li>Montajes Industriales</li>
+            <p>
+              Proporcionamos servicios completos de montaje industrial para la
+              instalación y configuración de maquinaria y sistemas en plantas de
+              producción. Nuestra experiencia garantiza una implementación sin
+              problemas y una puesta en marcha eficiente
+            </p>
+            <img
+              className="box"
+              src="https://img.freepik.com/free-photo/interior-view-steel-factory_1359-117.jpg?w=826&t=st=1694125028~exp=1694125628~hmac=d07535b0363906a945700a65ff690b444eec69da12085616eb231f5565b6b84a"
+              alt="montajes"
+              width="300"
+              height="300"
+            />
+          </div>
+          <div className="list-2-5">
+            <li>Puertas Eléctricas y Control de Acceso</li>
+            <p>
+              nstalamos sistemas de puertas eléctricas y control de acceso para
+              empresas e instalaciones industriales. Esto incluye puertas
+              automáticas, portones de seguridad y sistemas de acceso con
+              tarjeta o biométricos para garantizar la seguridad y la gestión
+              eficiente del flujo de personas.
+            </p>
+            <img
+              className="box"
+              src="https://img.freepik.com/free-photo/intercom-gate-front-residential-building_637285-1905.jpg?w=826&t=st=1694125294~exp=1694125894~hmac=fc7b2f24746c332c3f98a67d4a28b4407b53d09b9bb97d424ae9756fdeef0186"
+              alt="puerta"
+              width="300"
+              height="300"
+            />
+          </div>
+          <div className="list-2-6">
+            <li>Montajes Metalmecánicos</li>
+            <p>
+              Realizamos montajes metalmecánicos de alta calidad para
+              estructuras industriales, equipos y sistemas. Nuestros servicios
+              incluyen diseño, fabricación y montaje de componentes metálicos,
+              asegurando la resistencia y durabilidad de las instalaciones.
+            </p>
+            <img
+              src="https://img.freepik.com/free-photo/worker-operating-industrial-machine-metal-workshop_342744-268.jpg?w=900&t=st=1694188527~exp=1694189127~hmac=f9876038f084f0e67c25da96e71233c1888eaa32260a1919292e0745ec5d9860"
+              alt="metalmecanicos"
+              width="300"
+              height="300"
+            />
+          </div>
+          <div className="list-2-7">
             <li>Mantenimiento y Soporte Técnico</li>
             <p>
               Ofrecemos servicios de mantenimiento preventivo y correctivo para
@@ -107,6 +114,14 @@ const Servicios = () => {
               técnico está disponible para resolver cualquier problema de manera
               eficiente.
             </p>
+            <img
+              src="https://img.freepik.com/free-photo/man-builder-holds-protective-helmet-his-hands-part-body_169016-16067.jpg?w=900&t=st=1694187677~exp=1694188277~hmac=9b5e86a6f5527eabc4310bace7ef5919ec697f3d6971998cc9c7f2252174492e"
+              alt="mantenimiento"
+              width="300"
+              height="300"
+            />
+          </div>
+          <div className="list-2-8">
             <li>Consultoría y Desarrollo a Medida</li>
             <p>
               Brindamos servicios de consultoría en automatización industrial y
@@ -114,9 +129,15 @@ const Servicios = () => {
               procesos y sistemas. Además, desarrollamos soluciones a medida
               para abordar desafíos específicos de nuestros clientes.
             </p>
-          </ul>
+            <img
+              src="https://img.freepik.com/free-photo/still-life-business-roles-with-various-mechanism-pieces_23-2149352652.jpg?w=900&t=st=1694187938~exp=1694188538~hmac=f782c5091764eb549bd88d192fb2811b85c19cfccbec1e603106d042b895b102"
+              alt="consultaria"
+              width="300"
+              height="300"
+            />
+          </div>
         </ul>
-      
+      </ul>
     </div>
   );
 };
