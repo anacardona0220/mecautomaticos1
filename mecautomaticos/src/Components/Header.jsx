3b6cca95e328ac2aa383826c740b92f91a8dd92a
anacardona0220 <anamariacb0220@gmail.com>
2023-09-10 22:55:18 -0500

    update

--- mecautomaticos/src/Components/Header.jsx
+++ mecautomaticos/src/Components/Header.jsx
@@ -3,35 +3,37 @@ const Header = () => {
     <div className="header">
       <div className="container container-header">
         <div className="barra" />
-          <div className="logo">
+          <div className="logo lc">
             <h1 className="nombre-sitio">
-            Mecautomaticos <span>S.A.S</span>
+              Mecautomaticos <span>S.A.S</span>
             </h1>
           </div>
-          <div className="contacto">
+          <div className="contacto lc">
             <a href="tel:3136789101">3136789101</a>
             <nav className="navegacion">
-            <ul>
-
-              <li className="list" href="#">Nosotros</li>
-              <li className="list" href="#">Servicios</li>
-              <li className="list" href="#">Proyectos</li>
-              <li className="list" href="#">Contactanos</li>
-            </ul>
-              
+              <ul>
+                <li className="list" href="#">
+                  Nosotros
+                </li>
+                <li className="list" href="#">
+                  Servicios
+                </li>
+                <li className="list" href="#">
+                  Proyectos
+                </li>
+                <li className="list" href="#">
+                  Contactanos
+                </li>
+              </ul>
             </nav>
-
-
           </div>
         <div />
+        <div className="slogan">
+          <h1>Automatizando el futuro</h1>
+          <p>Soluciones inteligentes para la industria</p>
+        </div>
+        {/* <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShVd0xngEMMtA5DN6tRPdb0j98y-BZnZXzpQ&usqp=CAU" /> */}
       </div>
-      <div className="slogan">
-      <h1>Automatizando el futuro</h1>
-      <p>Soluciones inteligentes para la industria</p>
-
-      </div>
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShVd0xngEMMtA5DN6tRPdb0j98y-BZnZXzpQ&usqp=CAU" />
-      
     </div>
   );
 };
